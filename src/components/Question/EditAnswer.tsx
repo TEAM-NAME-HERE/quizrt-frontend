b70e0353b3c48ec19cb2b3a2a603973742bd52de
Abraham White <abelincoln.white@gmail.com>
2017-11-29 16:24:21 -0700

    Connect EditAnswer to api

--- src/components/Question/EditAnswer.tsx
+++ src/components/Question/EditAnswer.tsx
@@ -4,64 +4,190 @@ import IconButton from 'material-ui/IconButton';
 import DeleteIcon from 'material-ui-icons/Delete';
 import { FormGroup, FormControlLabel } from 'material-ui/Form';
 import TextField from 'material-ui/TextField';
+import { CreateAnswerMutation, AnswerFragment,
+         UpdateAnswerMutation, DeleteAnswerMutation, AnswerQuery } from '../../graphql/graphql';
+import { graphql, compose } from 'react-apollo';
+import { ChildProps } from 'react-apollo/types';
+import { Loading, Error } from '../Display';
 
-export interface Answer {
-  id: string;
-  description: string;
-  isCorrect: boolean;
-  order?: number;
-}
-
-export const emptyAnswer = (id: string): Answer => ({
+export const emptyAnswer = (id: string): AnswerFragment => ({
   id,
   description: '',
-  isCorrect: false
+  isCorrect: false,
+  __typename: 'AnswerNode'
 });
 
-export interface AnswerProps {
-  initialAnswer?: Answer;
-  onChange: (answer: Answer) => void;
-  onDelete: (answer: Answer) => void;
+export interface Props {
+  answer?: string;
+  onDelete: (id: string) => void;
+  question: string;
 }
 
-class EditAnswerItem extends React.Component<AnswerProps, Answer> {
-  static defauptProps = {
-    initialAnswer: emptyAnswer('')
-  };
-  constructor(p: AnswerProps) {
-    super(p);
+interface MutateProps {
+  onCreate: (answer: AnswerFragment, question: string) => Promise<AnswerFragment>;
+  onChange: (answer: AnswerFragment, question: string) => Promise<AnswerFragment>;
+  deleteAnswer: (id: string, callback: ((id: string) => void)) => Promise<void>;
+}
 
-    this.state = p.initialAnswer as Answer;
+const CREATE_MUTATION = require('../../graphql/mutations/CreateAnswer.graphql');
+const createMutation = graphql<CreateAnswerMutation, MutateProps & Props>(CREATE_MUTATION, {
+  props: ({ mutate }) => ({
+    onCreate: async (answer, question) => {
+      const results = await mutate!({
+        variables: {
+          description: answer.description,
+          isCorrect: answer.isCorrect,
+          question,
+        }
+      });
+
+      return results.data.createAnswer && results.data.createAnswer.answer;
+    }
+  } as Partial<MutateProps & Props>)
+});
+
+const UPDATE_MUTATION = require('../../graphql/mutations/UpdateAnswer.graphql');
+const updateMutation = graphql<UpdateAnswerMutation, MutateProps & Props>(UPDATE_MUTATION, {
+  props: ({ mutate }) => ({
+    onChange: async (answer, question) => {
+      const results = await mutate!({
+        variables: {
+          id: answer.id,
+          description: answer.description,
+          isCorrect: answer.isCorrect
+        }
+      });
+
+      return results.data.updateAnswer && results.data.updateAnswer.answer;
+    }
+  } as Partial<MutateProps & Props>)
+});
+
+const DELETE_MUTATION = require('../../graphql/mutations/DeleteAnswer.graphql');
+const deleteMutation = graphql<DeleteAnswerMutation, MutateProps & Props>(DELETE_MUTATION, {
+  props: ({ mutate }) => ({
+    deleteAnswer: async (id, cb) => {
+      await mutate!({
+        variables: {
+          id
+        }
+      });
+
+      cb(id);
+    }
+  } as Partial<MutateProps & Props>)
+});
+
+const ANSWER_QUERY = require('../../graphql/queries/Answer.graphql');
+const withAnswer = graphql<AnswerQuery, Props>(ANSWER_QUERY, {
+  skip: (props: Props) => !props.answer || props.answer === '',
+  options: (props) => ({
+    variables: { id: props.answer }
+  })
+});
+
+const compareObj = (a: Object, b: Object) => {
+  const aProps = Object.getOwnPropertyNames(a);
+  const bProps = Object.getOwnPropertyNames(b);
+  if (aProps.length !== bProps.length) {
+    return false;
+  }
+
+  for (const key in a) {
+    if (a.hasOwnProperty(key)) {
+      if (a[key] !== b[key]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+type AllProps = ChildProps<Props, AnswerQuery> & MutateProps;
+interface State {
+  answer: AnswerFragment;
+}
+
+class EditAnswerItem extends React.Component<AllProps, State> {
+  constructor(p: Props & MutateProps) {
+    super(p);
+    this.state = {answer: emptyAnswer('')};
+  }
+
+  componentWillReceiveProps(nextProps: MutateProps & ChildProps<Props, AnswerQuery>) {
+    if (nextProps.data && nextProps.data.answer) {
+      const data = nextProps.data.answer;
+      let same = compareObj(data, this.state);
+      if (!same) {
+        this.setState({ answer: data });
+      }
+    }
+  }
+
+  doCreate = async (a: AnswerFragment) => {
+    const { onCreate, question } = this.props;
+    const data = await onCreate(a, question);
+    this.setState({answer: data});
+  }
+
+  doChange = async (a: AnswerFragment) => {
+    const { onChange, question } = this.props;
+    /*const data =*/
+    await onChange(a, question);
+    // this.setState(data);
   }
 
   handleChange = (name: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
-    this.props.onChange({
-      ...this.state,
-      [name]: value
-    });
     this.setState({
-      ...this.state,
-      [name]: value
+      answer: {
+        ...this.state.answer,
+        [name]: value
+      }
     });
   }
 
+  saveAnswer = (a: AnswerFragment) => {
+    if (this.state.answer.id === '') {
+      this.doCreate(a);
+    } else {
+      this.doChange(a);
+    }
+  }
+
+  handleBlur = () => {
+    this.saveAnswer(this.state.answer);
+  }
+
   // tslint:disable-next-line:no-any
-  handleSwitch = (name: string) => (_: any, c: boolean) => {
-    this.props.onChange({
-      ...this.state,
-      [name]: c
-    });
+  handleSwitch = (_: any, c: boolean) => {
     this.setState({
-      ...this.state,
-      [name]: c
+      answer: {
+        ...this.state.answer,
+        isCorrect: c
+      }
+    });
+    this.saveAnswer({
+      ...this.state.answer,
+      isCorrect: c
     });
   }
 
   render() {
     // tslint:disable-next-line:no-any
-    const { initialAnswer: { id }} = this.props as any;
-    const { onDelete } = this.props;
+    const { answer: id, onDelete, deleteAnswer, data } = this.props;
+    const { answer } = this.state;
+    if (data) {
+      // tslint:disable-next-line:no-console
+      console.log(data, data.error);
+      if (data.loading) {
+        return <Loading />;
+      }
+      if (data.error) {
+        return <Error error={data.error} />;
+      }
+    }
     return (
       <div
         style={{
@@ -74,22 +200,23 @@ class EditAnswerItem extends React.Component<AnswerProps, Answer> {
         <TextField
           id={`answer-${id}-desc`}
           label="Description"
-          value={this.state.description}
+          value={answer.description}
           onChange={this.handleChange('description')}
+          onBlur={this.handleBlur}
           margin="normal"
         />
         <FormGroup>
           <FormControlLabel
             control={
             <Switch
-              checked={this.state.isCorrect}
-              onChange={this.handleSwitch('isCorrect')}
+              checked={answer.isCorrect}
+              onChange={this.handleSwitch}
               aria-label="isCorrect"
             />}
             label="Correct"
           />
         </FormGroup>
-        <IconButton onClick={() => onDelete(this.state)} aria-label="Delete Answer">
+        <IconButton onClick={() => deleteAnswer(answer.id, onDelete)} aria-label="Delete Answer">
           <DeleteIcon />
         </IconButton>
       </div>
@@ -97,4 +224,9 @@ class EditAnswerItem extends React.Component<AnswerProps, Answer> {
   }
 }
 
-export default EditAnswerItem;
+export default compose(
+  withAnswer,
+  createMutation,
+  updateMutation,
+  deleteMutation
+)(EditAnswerItem) as React.SFC<Props>;
--- src/graphql/graphql.ts
+++ src/graphql/graphql.ts
@@ -1,6 +1,90 @@
 /* tslint:disable */
 //  This file was automatically generated and should not be edited.
 
+export type CreateAnswerMutationVariables = {
+  description: string,
+  question: string,
+  isCorrect: boolean,
+};
+
+export type CreateAnswerMutation = {
+  createAnswer:  {
+    __typename: "CreateAnswerPayload",
+    answer:  {
+      __typename: "AnswerNode",
+      // The ID of the object.
+      id: string,
+      description: string,
+      isCorrect: boolean,
+    } | null,
+    clientMutationId: string | null,
+  } | null,
+};
+
+export type CreateQuestionMutationVariables = {
+  prompt: string,
+  quiz: string,
+};
+
+export type CreateQuestionMutation = {
+  createQuestion:  {
+    __typename: "CreateQuestionPayload",
+    question:  {
+      __typename: "QuestionNode",
+      // The ID of the object.
+      id: string,
+      prompt: string,
+    } | null,
+    clientMutationId: string | null,
+  } | null,
+};
+
+export type CreateQuizMutationVariables = {
+  name: string,
+  description: string,
+  isPrivate: boolean,
+  profile: string,
+};
+
+export type CreateQuizMutation = {
+  createQuiz:  {
+    __typename: "CreateQuizPayload",
+    quiz:  {
+      __typename: "QuizNode",
+      // The ID of the object.
+      id: string,
+      name: string,
+      description: string,
+      isPrivate: boolean,
+    } | null,
+    clientMutationId: string | null,
+  } | null,
+};
+
+export type DeleteAnswerMutationVariables = {
+  id: string,
+};
+
+export type DeleteAnswerMutation = {
+  deleteAnswer:  {
+    __typename: "DeleteAnswerPayload",
+    success: boolean | null,
+    clientMutationId: string | null,
+  } | null,
+};
+
+export type DeleteQuestionMutationVariables = {
+  id: string,
+};
+
+export type DeleteQuestionMutation = {
+  deleteQuestion:  {
+    __typename: "DeleteQuestionPayload",
+    success: boolean | null,
+    clientMutationId: string | null,
+  } | null,
+};
+
 export type LoginUserMutationVariables = {
   email: string,
   password: string,
@@ -49,6 +133,59 @@ export type CreateUserMutation = {
   } | null,
 };
 
+export type UpdateAnswerMutationVariables = {
+  id: string,
+  description?: string | null,
+  isCorrect?: boolean | null,
+};
+
+export type UpdateAnswerMutation = {
+  updateAnswer:  {
+    __typename: "UpdateAnswerPayload",
+    answer:  {
+      __typename: "AnswerNode",
+      // The ID of the object.
+      id: string,
+      description: string,
+      isCorrect: boolean,
+    } | null,
+    clientMutationId: string | null,
+  } | null,
+};
+
+export type UpdateQuestionMutationVariables = {
+  id: string,
+  prompt?: string | null,
+};
+
+export type UpdateQuestionMutation = {
+  updateQuestion:  {
+    __typename: "UpdateQuestionPayload",
+    question:  {
+      __typename: "QuestionNode",
+      // The ID of the object.
+      id: string,
+      prompt: string,
+    } | null,
+    clientMutationId: string | null,
+  } | null,
+};
+
+export type AnswerQueryVariables = {
+  id: string,
+};
+
+export type AnswerQuery = {
+  // The ID of the object
+  answer:  {
+    __typename: "AnswerNode",
+    // The ID of the object.
+    id: string,
+    description: string,
+    isCorrect: boolean,
+  } | null,
+};
+
 export type ProfileWithQuizzesQueryVariables = {
   id: string,
 };
@@ -74,6 +211,32 @@ export type ProfileWithQuizzesQuery = {
           name: string,
           description: string,
           isPrivate: boolean,
+        } | null,
+      } | null >,
+    } | null,
+  } | null,
+};
+
+export type QuestionQueryVariables = {
+  id: string,
+};
+
+export type QuestionQuery = {
+  // The ID of the object
+  question:  {
+    __typename: "QuestionNode",
+    // The ID of the object.
+    id: string,
+    prompt: string,
+    answerSet:  {
+      __typename: "AnswerNodeConnection",
+      edges:  Array< {
+        __typename: "AnswerNodeEdge",
+        // The item at the end of the edge
+        node:  {
+          __typename: "AnswerNode",
+          // The ID of the object.
+          id: string,
         } | null,
       } | null >,
     } | null,
@@ -111,6 +274,14 @@ export type UserWithProfilesQuery = {
   } | null,
 };
 
+export type AnswerFragment = {
+  __typename: "AnswerNode",
+  // The ID of the object.
+  id: string,
+  description: string,
+  isCorrect: boolean,
+};
+
 export type ProfileFragment = {
   __typename: "ClassProfileNode",
   // The ID of the object.
@@ -118,6 +289,13 @@ export type ProfileFragment = {
   name: string,
   description: string,
   isPrivate: boolean,
+};
+
+export type QuestionFragment = {
+  __typename: "QuestionNode",
+  // The ID of the object.
+  id: string,
+  prompt: string,
 };
 
 export type QuizFragment = {
